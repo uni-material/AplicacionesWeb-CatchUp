0f1e5f55a184594e6e43833e82a9b88a85b16609
Ro <u202213765@upc.edu.pe>
2024-09-12 19:42:13 -0500

    feat: implemented news service.

---
+++ src/news/services/news-api.service.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import {LogoApiService} from "@/shared/services/news-api.service.js";
+
+
+const http = axios.create({
+    baseURL: 'https://newsapi.org/v2',
+});
+
+
+const NES_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+
+
+export class NewsApiService{
+    apiKey = NES_API_KEY;
+    logoApi = new LogoApiService();
+
+    //metodos
+    getSources(){
+        return http.get(`top-headlines/sources?apiKey=${this.apiKey}`)
+    }
+
+    getArticlesForSource(sourceId){
+        return http.get( `top-headlines?sources=${sourceId}&apiKey=${this.apiKey}`)
+    }
+
+    getUrlToLogo(source){
+        return this.logoApi.getUrlToLogo(source);
+    }
+
+}
+}
